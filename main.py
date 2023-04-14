283b4ac4d741ddb0c62aee29f272d700fcb1f846
Sahil <leviack629@gmail.com>
2023-04-15 01:02:13 +0530

    scipy

---
+++ main.py
@@ -0,0 +1,7 @@
+#SciPy is a scientific computation library that uses NumPy underneath.
+
+# SciPy stands for Scientific Python.
+import scipy
+from scipy import constants
+print(constants.liter) #returns 1 liter as cubic meters
+print(scipy.__version__)
