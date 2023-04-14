7c4be76d57a5fe340775bba84556e579e1bf7b5e
Sahil <leviack629@gmail.com>
2023-04-15 01:02:48 +0530

    constants

---
+++ constants.py
@@ -0,0 +1,12 @@
+from scipy import constants
+
+print(constants.pi)
+
+print()
+print(dir(constants))
+print(constants.kilo) #returns in meters
+
+print(constants.kibi)
+print(constants.week)
+
+print(constants.lbf)
